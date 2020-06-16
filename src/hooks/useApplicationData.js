96ce82022ecdb18d6acd6d83b4ac511d690cf974
JoshLampen <joshlampen2@gmail.com>
2020-06-16 13:10:31 -0700

    Moved reducer function to a separate file and added a test for it

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -1,55 +1,7 @@
 import React, { useReducer, useEffect } from "react";
 import axios from "axios";
 import { getAppointmentsForDay } from "helpers/selectors";
-
-const SET_DAY = "SET_DAY";
-const SET_APPLICATION_DATA = "SET_APPLICATION_DATA";
-const SET_INTERVIEW = "SET_INTERVIEW";
-
-function reducer(state, action) {
-  switch (action.type) {
-    case SET_DAY:
-      return { ...state, ...action.payload }
-    case SET_APPLICATION_DATA:
-      return { ...state, ...action.payload }
-    case SET_INTERVIEW:
-      let newDay = state.days.filter(day => day.appointments.includes(action.id))[0];
-
-      if (!(state.appointments[action.id].interview && action.interview)) {
-        if (action.interview) {
-          newDay.spots--;
-        } else {
-          newDay.spots++;
-        }
-      }
-
-      const days = state.days;
-
-      days.map(day => {
-        if (day.id === newDay.id) {
-          day = newDay
-        }
-      });
-
-      const interview = action.interview ? { ...action.interview } : null;
-
-      const appointment = {
-        ...state.appointments[action.id],
-        interview,
-      };
-
-      const appointments = {
-        ...state.appointments,
-        [action.id]: appointment,
-      };
-
-      return { ...state, appointments, days }
-    default:
-      throw new Error(
-        `Tried to reduce with unsupported action type: ${action.type}`
-      );
-  }
-}
+import reducer, { SET_DAY, SET_APPLICATION_DATA, SET_INTERVIEW } from "reducers/application";
 
 export default function useApplicationData() {
   const [state, dispatch] = useReducer(reducer, {
---
+++ src/reducers/application.js
@@ -0,0 +1,50 @@
+const SET_DAY = "SET_DAY";
+const SET_APPLICATION_DATA = "SET_APPLICATION_DATA";
+const SET_INTERVIEW = "SET_INTERVIEW";
+
+export { SET_DAY, SET_APPLICATION_DATA, SET_INTERVIEW }
+
+export default function reducer(state, action) {
+  switch (action.type) {
+    case SET_DAY:
+      return { ...state, ...action.payload }
+    case SET_APPLICATION_DATA:
+      return { ...state, ...action.payload }
+    case SET_INTERVIEW:
+      let newDay = state.days.filter(day => day.appointments.includes(action.id))[0];
+
+      if (!(state.appointments[action.id].interview && action.interview)) {
+        if (action.interview) {
+          newDay.spots--;
+        } else {
+          newDay.spots++;
+        }
+      }
+
+      const days = state.days;
+
+      days.map(day => {
+        if (day.id === newDay.id) {
+          day = newDay
+        }
+      });
+
+      const interview = action.interview ? { ...action.interview } : null;
+
+      const appointment = {
+        ...state.appointments[action.id],
+        interview,
+      };
+
+      const appointments = {
+        ...state.appointments,
+        [action.id]: appointment,
+      };
+
+      return { ...state, appointments, days }
+    default:
+      throw new Error(
+        `Tried to reduce with unsupported action type: ${action.type}`
+      );
+  }
+}
